b075203d6ef41b62ede31ec4ff374335ef2f1360
RK <riku.kuu0421@gmail.com>
2022-10-29 13:35:04 +0900

    generator完成

---
+++ Assets/ohashi/script/EnemyGenerator.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// enemyのスポーンとリスポーンのクラス
+/// </summary>
+public class EnemyGenerator : MonoBehaviour
+{
+    [SerializeField] Transform[] _spawnPos = default;
+    [SerializeField] testEnemy[] _enemies;
+    [SerializeField] int _maxPos;
+    [SerializeField] float interval = 1f;
+    List<Transform> _posList = new List<Transform>();
+    bool _rePopInterval = false;
+    int _gCount = 0;
+
+    void Start()
+    {
+        foreach (var t in _spawnPos)
+        {
+            //Listに全部追加
+            _posList.Add(t);
+        }
+    }
+    void Spawn()
+    {
+        int r1 = Random.Range(0, _posList.Count - 1);
+
+        Transform pos = _posList[r1];
+
+        int r2 = Random.Range(0, _enemies.Length);
+
+        var enemyB = Instantiate(_enemies[r2]);
+        enemyB.Set(this, pos);//参照先をセット
+        enemyB.transform.position = pos.position;
+        enemyB.transform.rotation = pos.rotation;
+        _posList.Remove(pos); //削除
+    }
+    public void EnemyDestroy(Transform t)
+    {
+        _posList.Add(t);//Destroyしたenemyがスポーンした位置をリストに追加
+        _gCount--;
+    }
+    void Update()
+    {
+        if (!_rePopInterval && _gCount <= _maxPos)
+        {
+            _gCount++;
+            StartCoroutine(SpawnInterval());
+            _rePopInterval = true;
+        }
+    }
+    IEnumerator SpawnInterval()
+    {
+        
+        yield return new WaitForSeconds(interval);
+        Spawn();
+        _rePopInterval = false;
+    }
+}
