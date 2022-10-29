07f510f9de0021bfbc6c2fb2e4dae005658afaa8
miyn <20miyu03@gmail.com>
2022-10-29 14:13:09 +0900

    エネミーを押したらエネミーが消える、スコア加算

---
+++ Assets/hioki/script/Enemy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Enemy : MonoBehaviour, IPointerClickHandler
+{
+    EnemyGenerator _enemyG;
+    Transform _spawnPos;
+    [SerializeField]ScoreAndScene _scoreAndScene;
+    [Header("ÉXÉRÉA")]
+    [SerializeField] int _score;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        var go = eventData.pointerCurrentRaycast.gameObject;
+        if(go.name == "a")
+        {
+            Destroy(go);
+        }
+    }
+    private void OnDestroy()
+    {
+        _scoreAndScene._score += _score;
+        _enemyG.EnemyDestroy(_spawnPos);
+    }
+    public void Set(EnemyGenerator eg, Transform t)
+    {
+        _enemyG = eg;
+        _spawnPos = t;
+
+    }
+}
