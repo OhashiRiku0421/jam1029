dab624bb400d4b0246ead0266f1ba419cd1ba454
NakamuraTakuto <rulitaku@icloud.com>
2022-10-29 13:22:50 +0900

    score変数宣言

---
+++ Assets/nakamura/Script/ScoreAndScene.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreAndScene : MonoBehaviour
+{
+    [SerializeField, Header("ƒXƒRƒA•Ï”")] public int _score = 0;
+    static int scoreData;
+}
